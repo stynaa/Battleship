69880dab3f4b118390e77b3f8d62e7eaf593e6e4
stynaa <33501062+stynaa@users.noreply.github.com>
2017-11-10 18:23:47 -0700

    changed negative numbers to respective variables

--- Computer.java
+++ Computer.java
@@ -1,61 +1,71 @@
+//package battleship;
+
 import java.util.Random;
 
-//new edits blahblahblah
-public class Computer {
-  int MAXROW = 10;
-  int MAXCOL = 10;
-  int[][] board = new int[MAXCOL][MAXROW]; //initialized in setComputer, 5 by 5 board
-  int[] coord = new int[2]; //used to store x,y coordinate for set up and game play
+public class Computer extends Board{
   char direction; //for setPlayer and setComputer, for direction of ships
 
+  //currently poorly implemented booleans
   boolean lastGood = false; //Boolean for checking around the shot location and which direction
   boolean trueN = false;
   boolean trueS = false;
   boolean trueE = false;
   boolean trueW = false;
 
+  //Recent idea variables from TA feedback
+  //Instead of holding arbitrary numbers, there will be variable names that provide useful information
+  public static final int shotWEIGH = 0;
+  public static final int chooseDirection = -10;
+  public static final int shotRAND = -100;
+  public static final int shotNorth = -2;
+  public static final int shotEast = -3;
+  public static final int shotWest = -4;
+  public static final int shotSouth = -5;
+
+  //An array which will hold a copy of the original shot
   int[] originalShot = new int[2];
 
-
+  //maybe move this to board class
   boolean win = false; //to determine winner in winCheck()
   int playerHitCounter=0;
 
   public Computer(){
-    for(int row=0;row<MAXROW;row++){
-      for(int column=0;column<MAXCOL;column++){
-        board[row][column]=0;
-      }
-    }
+   super();
   }
 
   //Gets move of Computer
   //Stores move in coord
   public void getMove(Player p1) {
-    Random rand = new Random(); // Initalize random seed 
-    
-    
-    if (p1.boardState == 0) { // Weight random
+    Random rand = new Random(); // Initalize random
+
+
+    if (p1.boardState == shotWEIGH) { // Weight random
       int[] boardChoice;
-      //boardChoice[] = {1,2,2,3,3,3,4,4,5}; //Currently non-dynamic for 5
+      //May implement a more sophisticated weight shot
+      //Central and odd lines are slightly favoured during targetting
+      //
       boardChoice = new int[] {0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9};
       coord[1] = boardChoice[rand.nextInt(boardChoice.length)]; //X
       coord[0] = boardChoice[rand.nextInt(boardChoice.length)]; //Y
+      //Troubleshooting messages
+      //need to figure out how to print this to GUI
       System.out.println("Computer fires at: " + coord[1] + ", " + coord[0] + ".");
       System.out.println("RANDOM");
-    } 
+    }
 
-    else if (p1.boardState == -100) { //Normal random
+    else if (p1.boardState == shotRAND) { //Normal random, previously used as basic firing, DO NOT USE
       coord[1] = rand.nextInt(MAXROW); //X
       coord[0] = rand.nextInt(MAXCOL); //Y
       System.out.println("Computer fires at: " + coord[1] + ", " + coord[0] + ".");
     }
 
-    else if (p1.boardState == -2) { // Hit, checking Y-North
+    else if (p1.boardState == shotNorth) { // Hit, checking Y-North
     //I am not sure if I need to do this "copy" correctly
     //Temporary copy to ensure not out of bounds
       int temp[] = new int[2];
-      temp[1] = p1.coordStored[1];      //X? Unsure
+      temp[1] = p1.coordStored[1];      //X?
       temp[0] = p1.coordStored[0] - 1;  //Y?
+      //Troubleshooting for the  temporary array
       System.out.println(p1.board.length);
       System.out.println(temp[1]);
       System.out.println(temp[0]);
@@ -63,16 +73,20 @@ public class Computer {
         coord[1] = p1.coordStored[1];
         coord[0] = p1.coordStored[0] - 1;
         trueN = true;
+        //
         System.out.println("Computer fires at: " + coord[1] + ", " + coord[0] + ".");
         System.out.println("North");
       }
+      //My poor attempt at recursion, just a temporary else
+      //Tracing through this, it's quite pointless
+      //Will require assistance for properly implementing it
       else {
-        p1.boardState = -10;
+        p1.boardState = chooseDirection;
         getMove(p1);
       }
     }
 
-    else if (p1.boardState == -3) { // Hit, checking X-East
+    else if (p1.boardState == shotEast) { // Hit, checking X-East
       int temp[] = new int[2];
       temp[1] = p1.coordStored[1] + 1;      //X
       temp[0] = p1.coordStored[0];          //Y
@@ -87,19 +101,19 @@ public class Computer {
         System.out.println("East");
       }
             else {
-              p1.boardState = -10;
+              p1.boardState = chooseDirection;
         getMove(p1);
       }
     }
 
-    else if (p1.boardState == -4) { // Hit, checking X-West
+    else if (p1.boardState == shotWest) { // Hit, checking X-West
       int temp[] = new int[2];
       temp[1] = p1.coordStored[1] - 1;      //X
       temp[0] = p1.coordStored[0];          //Y
       System.out.println(p1.board.length);
       System.out.println(temp[1]);
       System.out.println(temp[0]);
-//      if (temp [1] < p1.coord.getLength) { 
+//      if (temp [1] < p1.coord.getLength) {
       if (temp[1] >= 0) {
         coord[1] = p1.coordStored[1] - 1;
         coord[0] = p1.coordStored[0];
@@ -108,19 +122,19 @@ public class Computer {
         System.out.println("West");
       }
             else {
-              p1.boardState = -10;
+              p1.boardState = chooseDirection;
         getMove(p1);
       }
     }
 
-    else if (p1.boardState == -5) { // Hit, checking Y-South
+    else if (p1.boardState == shotSouth) { // Hit, checking Y-South
       int temp[] = new int[2];
       temp[1] = p1.coordStored[1];          //X
       temp[0] = p1.coordStored[0] + 1;      //Y
       System.out.println(p1.coord.length);
       System.out.println(temp[1]);
       System.out.println(temp[0]);
-//      if (temp [1] < p1.coord.getLength) { 
+//      if (temp [1] < p1.coord.getLength) {
       if (temp[0] <= p1.board.length) {
         coord[1] = p1.coordStored[1];
         coord[0] = p1.coordStored[0] + 1;
@@ -129,12 +143,20 @@ public class Computer {
         System.out.println("South");
       }
       else {
-        p1.boardState = -10;
+        p1.boardState = chooseDirection;
         getMove(p1);
       }
 
     }
-    else if (p1.boardState == -10) {  //Barrage
+    else if (p1.boardState == chooseDirection) {  //Barrage
+    	//Currently not implemented as desired
+    	/*
+    	Missing:
+    	Saved original shot
+    	Ship hit data (if possible)
+    	A counter for how long to 'barrage' for
+    	Implementation will be based on desired action
+    	*/
       int tempDir = rand.nextInt(4);
       p1.boardState = -(tempDir + 2);
       System.out.println("Direction " + p1.boardState);
@@ -144,17 +166,18 @@ public class Computer {
     }
 
     else {
-      p1.boardState = -10;
+      p1.boardState = chooseDirection;
       getMove(p1); //This is a really messy protocol
-    }    
-      
+    }
+
   }
 
   public boolean HitorMiss(Player p1) {
-    //Takes in computer’s move and see if hit’s player's game pieces
+    //Takes in computer�s move and see if hit�s player's game pieces
     //uses coord from p1 and computer's board
     //checks if p1 coords hit computer's board
     //alters computer's board accordingly
+    //firing protocol is selected accordingly
     if (board[p1.coord[0]][p1.coord[1]] == 1 )
       {
       board[p1.coord[0]][p1.coord[1]] = 3;
