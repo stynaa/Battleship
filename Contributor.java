113b1c2cd93c7b4d1cb30c02f25600bfb2e07f20
henryeng <32807308+henryeng@users.noreply.github.com>
2017-11-17 14:25:37 -0700

    Add files via upload

---
+++ Contributor.java
@@ -0,0 +1,48 @@
+
+public class Contributor 
+{
+	private boolean win;
+	private boolean endGame;
+	private boolean shotHit;
+	private int[] shot;
+	
+	public int[] getShot()
+	{
+		return null;
+	}
+	
+	public void setShot()
+	{
+		
+	}
+	
+	public boolean HitOrMiss()
+	{
+		return true;
+	}
+	
+	public boolean winCheck(Computer c1, Player p1)
+	{
+		return true;
+	}
+	
+	public boolean getWin()
+	{
+		return win;
+	}
+	
+	public void setWin()
+	{
+		
+	}
+	
+	public boolean getEndGame()
+	{
+		return endGame;
+	}
+	
+	public void setEndGame()
+	{
+		
+	}	
+}
---
+++ Ship.java
@@ -0,0 +1,33 @@
+
+public class Ship 
+{
+	private char Direction;
+	private int[] coord = new int[2];
+	private int SHIPCODE;
+	private final static int CARRIER = 5;
+	private final static int BATTLESHIP = 6;
+	private final static int CRUISER = 7;
+	private final static int SUBMARINE = 8;
+	private final static int DESTROYER = 9;
+	
+	public int getShipSize(int shipCode)
+	{
+		return 0;
+	}
+	
+	public Ship getShip()
+	{
+		return null;
+	}
+	
+	public void setShip()
+	{
+		
+	}
+	
+	public boolean isShipDestroyed()
+	{
+		return true;
+	}
+
+}
