695c8fe2a0c29b61af2317cc6bd3031ef23a24c1
shawnamckay <shawnammckay@gmail.com>
2017-11-28 08:38:59 -0700

    Added labels for Player/Computer Boards

--- BattleFrameGUI.java
+++ BattleFrameGUI.java
@@ -12,12 +12,15 @@ public class BattleFrameGUI extends JPanel{
     public BattleFrameGUI(Player player, Computer computer, ActionListener listener) {
         JPanel content = new JPanel();
         JPanel gameBoard = new JPanel();
+        JPanel boardLabels = new JPanel();
         playerGrid= new Buttons(player, listener);
         computerGrid= new Buttons(computer, listener);
         setPreferredSize(new Dimension(1000,800));
         content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
         content.setAlignmentX(Component.CENTER_ALIGNMENT);
         setVisible(true);
+        boardLabels=createBoardLabels(boardLabels);
+        content.add(boardLabels);
         gameBoard.setLayout(new GridLayout(1,2,10,10));
         gameBoard.add(playerGrid);
         gameBoard.add(computerGrid);
@@ -25,6 +28,21 @@ public class BattleFrameGUI extends JPanel{
         info = new InfoPanel();
         content.add(info);
         add(content);
+    }
+
+    public JPanel createBoardLabels(JPanel boardLabels){
+        boardLabels.setLayout(new BoxLayout(boardLabels,BoxLayout.X_AXIS));
+        JLabel playerLbl = new JLabel("Player");
+        JLabel computerLbl = new JLabel("Computer");
+        playerLbl.setFont(new Font("Serif", Font.BOLD, 18));
+        computerLbl.setFont(new Font("Serif", Font.BOLD, 18));
+        boardLabels.add(Box.createHorizontalGlue());
+        boardLabels.add(playerLbl);
+        boardLabels.add(Box.createHorizontalGlue());
+        boardLabels.add(Box.createHorizontalGlue());
+        boardLabels.add(computerLbl);
+        boardLabels.add(Box.createHorizontalGlue());
+        return boardLabels;
     }
 
     //accessor for the computer Grid button array
