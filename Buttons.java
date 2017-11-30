7ea9bdbb5be44003383c20deac4fe7448c7f84c8
shawnamckay <shawnammckay@gmail.com>
2017-11-30 16:22:23 -0700

    Added water image to game

--- Buttons.java
+++ Buttons.java
@@ -15,6 +15,7 @@ public class Buttons extends  JPanel{
     private JButton[][] button = new JButton[MAXROW][MAXCOL];
     public JPanel contributorGrid= new JPanel();
     public ImageIcon fire = new ImageIcon(getFire()); //initialized as an instance variable to increase application speed
+    public ImageIcon water = new ImageIcon(getWater());
 
     //board value constants
     public static final int EMPTY=0;
@@ -38,7 +39,7 @@ public class Buttons extends  JPanel{
                 button[r][c].addActionListener(listener);
                 button[r][c].setActionCommand(aPlayer.getClass().getName()+" "+Integer.toString(r)+ " "+Integer.toString(c));
                 button[r][c].setBackground(Color.blue);
-//                button[r][c].setIcon(new ImageIcon(getWater()));
+                button[r][c].setIcon(water);
                 contributorGrid.add(button[r][c]);
             }
         }
@@ -77,38 +78,38 @@ public class Buttons extends  JPanel{
     }
 
 
-//
-//    //method to grab the water background from an image file.
-//    //IO Exception is handled through creation of a new image that is blue
-//    //If the new image creation fails a stack trace is printed to the console.
-//    public Image getWater() {
-//        BufferedImage waterImage = null;
-//        try {
-//            waterImage = ImageIO.read(new File("wave.jpg"));
-//        }
-//        catch(IOException ioe) {
-//            try {
-//                waterImage = new BufferedImage(
-//                        48, 48, BufferedImage.TYPE_INT_RGB);
-//
-//                File f = new File("Water.png");
-//                int r = 0;
-//                int g = 0;
-//                int b = 255;
-//                int col = r | g | b;
-//                for (int x = 0; x < 48; x++) {
-//                    for (int y = 0; y < 48; y++) {
-//                        waterImage.setRGB(x, y, col);
-//                    }
-//                }
-//                ImageIO.write(waterImage, "PNG", f);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        return waterImage;
-//    }
-//
+
+    //method to grab the water background from an image file.
+    //IO Exception is handled through creation of a new image that is blue
+    //If the new image creation fails a stack trace is printed to the console.
+    public Image getWater() {
+        BufferedImage waterImage = null;
+        try {
+            waterImage = ImageIO.read(new File("wave.jpg"));
+        }
+        catch(IOException ioe) {
+            try {
+                waterImage = new BufferedImage(
+                        45, 45, BufferedImage.TYPE_INT_RGB);
+
+                File f = new File("Water.png");
+                int r = 0;
+                int g = 0;
+                int b = 255;
+                int col = r | g | b;
+                for (int x = 0; x < 45; x++) {
+                    for (int y = 0; y < 45; y++) {
+                        waterImage.setRGB(x, y, col);
+                    }
+                }
+                ImageIO.write(waterImage, "PNG", f);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return waterImage;
+    }
+
 
     //updates the colors of the buttons to show the user where their ships are placed
     //Also shows if the user has hit or missed during the gamePlay
@@ -116,8 +117,10 @@ public class Buttons extends  JPanel{
         for (int r = 0; r < MAXROW; r++) {
             for (int c = 0; c < MAXCOL; c++) {
                 if (aPlayer.getBoard().getBoard()[r][c] == EMPTY) { //empty
+                    button[r][c].setIcon(water);
                     button[r][c].setBackground(Color.blue);
                 } else if (aPlayer.getBoard().getBoard()[r][c] == MISS) { //Carrier
+                    button[r][c].setIcon(null);
                     button[r][c].setBackground(Color.green);
                 } else if (aPlayer.getBoard().getBoard()[r][c] == HIT) { //Carrier
                    button[r][c].setIcon(fire);
@@ -125,14 +128,19 @@ public class Buttons extends  JPanel{
                 }
 //                if(aPlayer.getClass().getName().equals("Human")){
                 else if (aPlayer.getBoard().getBoard()[r][c] == CARRIER) { //Carrier
+                    button[r][c].setIcon(null);
                     button[r][c].setBackground(Color.yellow);
                 } else if (aPlayer.getBoard().getBoard()[r][c] == BATTLESHIP) { //Battleship
+                    button[r][c].setIcon(null);
                     button[r][c].setBackground(Color.cyan);
                 } else if (aPlayer.getBoard().getBoard()[r][c] == CRUISER) {//Cruiser
+                    button[r][c].setIcon(null);
                     button[r][c].setBackground(Color.orange);
                 } else if (aPlayer.getBoard().getBoard()[r][c] == SUBMARINE) { //Submarine
+                    button[r][c].setIcon(null);
                     button[r][c].setBackground(Color.pink);
                 } else if (aPlayer.getBoard().getBoard()[r][c] == DESTROYER) { //Destroyer
+                    button[r][c].setIcon(null);
                     button[r][c].setBackground(Color.magenta);
                 }
             }
@@ -144,6 +152,7 @@ public class Buttons extends  JPanel{
         colorButtons(aPlayer);
         int row= coord[0];
         int col= coord[1];
+        button[row][col].setIcon(null);
         button[row][col].setBackground(Color.green);
             }
 
