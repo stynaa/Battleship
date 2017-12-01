317b0afd2a22e3aa3627d2fb49ee34b3ac8d90aa
shawnamckay <shawnammckay@gmail.com>
2017-12-01 14:06:58 -0700

    added javadoc

--- Human.java
+++ Human.java
@@ -1,16 +1,22 @@
-
+/**
+ * Human Class to represent the user's game play
+ *  Class is maintained help with readability in the Controller classes
+ */
 public class Human extends Player
 {
-
+	/**
+	 * Default constructor, calls the default constructor in the Player Class
+	 */
 	public Human()
 	{
 		super();
 	}
 
+	/**
+	 * Copy constructor, calls the copy constructor in the Player Class
+	 */
 	public Human(Human toCopy){
 		super(toCopy);
 	}
-
-
 
 }
