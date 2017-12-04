64a452fe4dcd82e335c0be4cdf264b697f18816a
tonyychan <tjchan21@gmail.com>
2017-12-04 10:14:54 -0700

    Update BattleFrameGUI.java

--- BattleFrameGUI.java
+++ BattleFrameGUI.java
@@ -9,22 +9,24 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class BattleFrameGUI extends JPanel{
-    private InfoPanel info; //info panel that explains the current game status.
+    
+    private InfoPanel info; //info panel that explains the current game status
     private Buttons playerGrid; //Button array displaying the player's board
     private Buttons computerGrid; //Button array displaying the computer's board
 
     //initializes the game play views
     public BattleFrameGUI(Human human, Computer computer, ActionListener listener) {
+        
         JPanel content = new JPanel();
         JPanel gameBoard = new JPanel();
         JPanel boardLabels = new JPanel();
-        playerGrid= new Buttons(human, listener);
-        computerGrid= new Buttons(computer, listener);
+        playerGrid = new Buttons(human, listener);
+        computerGrid = new Buttons(computer, listener);
         setPreferredSize(new Dimension(1000,800));
         content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
         content.setAlignmentX(Component.CENTER_ALIGNMENT);
         setVisible(true);
-        boardLabels=createBoardLabels(boardLabels);
+        boardLabels = createBoardLabels(boardLabels);
         content.add(boardLabels);
         gameBoard.setLayout(new GridLayout(1,2,10,10));
         gameBoard.add(playerGrid);
@@ -36,6 +38,7 @@ public class BattleFrameGUI extends JPanel{
     }
 
     public JPanel createBoardLabels(JPanel boardLabels){
+        
         boardLabels.setLayout(new BoxLayout(boardLabels,BoxLayout.X_AXIS));
         JLabel playerLbl = new JLabel("Human");
         JLabel computerLbl = new JLabel("Computer");
@@ -58,7 +61,6 @@ public class BattleFrameGUI extends JPanel{
 
     //accessor for the Info Panel
     public InfoPanel getInfoPanel() {return info;}
-
 
 }
 
