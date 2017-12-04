a3d187333c16bce07df179e84fd5418809876dce
stynaa <33501062+stynaa@users.noreply.github.com>
2017-12-04 11:49:57 -0700

    cleaned and documented - should be finished
    cleaned and documented player class:
    - added documentation to each method
    - changed variable names so that they're more "meaningful" lol
    - cleaned up some code
    - checked that it compiles and everything works normally after changes

--- Player.java
+++ Player.java
@@ -1,91 +1,103 @@
 
 public abstract class Player {
-	
 	private boolean win = false;
-	private Board b;
+	private Board board;
 	int[] shot = new int[2];
 	public static int MAXROW = 10;
 	public static int MAXCOL = 10;
 	private char direction;
-	private String message = "";
-	//maybe add another board to track the shots on the opponent's board??
+	private String message="";
 
+/*
+Player Constructor - nothing taken in
+creates a new board
+*/
 	public Player() {
-		b = new Board();
+		board = new Board();
 	}
 
+/*
+Player Constructor - takes in a Player
+saves board and shot from the player to copy
+*/
 	public Player(Player toCopy){
-		b=new Board(toCopy.getBoard());
+		board = new Board(toCopy.getBoard());
 		setShot(toCopy.getShot());
 	}
 
+/*
+getShot
+returns the shot currently saved in player class
+*/
 	public int[] getShot() {
-		//not passed by reference
-		int[] copyShot = new int[2];
-		copyShot[0] = shot[0];
-		copyShot[1] = shot[1];
-
-		return copyShot;
+		return shot;
 	}
 
+/*
+setShot
+takes in a shot coordinate and saves it as player's shot
+*/
 	public void setShot(int[] shotCoordinate) {
 		shot[0] = shotCoordinate[0];
 		shot[1] = shotCoordinate[1];
 	}
 
+/*
+HitOrMiss - nothing taken in
+Gets the opponent's shot (saved in shot)
+Checks current state of player board at the coordinate of the opponent's shot
+Returns true if the player board at opponent's shot coordinate is equal to a ship value
+alters player's board accordingly to record that the ship has been shot
+*/
 	public boolean HitOrMiss() {
-		
-		//Takes in computers move and see if hits player's game pieces
-		//uses coord from p1 and computer's board
-		//checks if p1 coords hit computer's board
-		//alters computer's board accordingly
-
 		int[] oppShot=getShot();
 
 		boolean oppHit = false;
-		if (b.getBoard()[oppShot[0]][oppShot[1]] == 1) {
-			
-			b.getBoard()[oppShot[0]][oppShot[1]] = 3;
+		if (board.getBoard()[oppShot[0]][oppShot[1]] == 1) {
+
+			board.getBoard()[oppShot[0]][oppShot[1]] = 3;
 			oppHit = true;
-			
-			//hitCounter();
 
 		} else {
-			//make sure something else checks validity of shot
-			b.getBoard()[oppShot[0]][oppShot[1]] = 2;
+			board.getBoard()[oppShot[0]][oppShot[1]] = 2;
 			oppHit = false;
 		}
 		return oppHit;
 	}
 
-	public boolean HitOrMiss(int[] shot, Player b){
-		
+	/*
+	HitOrMiss - takes in opponent's shot and current player
+	Checks current state of current player board at the coordinate of the opponent's shot
+	Returns true if the player board at opponent's shot coordinate is equal to a ship value
+	sets the message according to the result
+	*/
+	public boolean HitOrMiss(int[] oppShot, Player curPlayer){
 		boolean oppHit = false;
-		
-		if (b.getBoard().getBoard()[shot[0]][shot[1]] == 5||b.getBoard().getBoard()[shot[0]][shot[1]] == 6||
-				b.getBoard().getBoard()[shot[0]][shot[1]] == 7||b.getBoard().getBoard()[shot[0]][shot[1]] == 8||
-				b.getBoard().getBoard()[shot[0]][shot[1]] == 9) {
+		if (curPlayer.getBoard().getBoard()[oppShot[0]][oppShot[1]] == 5||curPlayer.getBoard().getBoard()[oppShot[0]][oppShot[1]] == 6||
+				curPlayer.getBoard().getBoard()[oppShot[0]][oppShot[1]] == 7||curPlayer.getBoard().getBoard()[oppShot[0]][oppShot[1]] == 8||
+				curPlayer.getBoard().getBoard()[oppShot[0]][oppShot[1]] == 9) {
 
-//			b.getBoard().getBoard()[shot[0]][shot[1]] = 3;
 			oppHit = true;
 			setMessage("Hit!");
-			//hitCounter();
 
 		} else {
-			//make sure something else checks validity of shot
-//			b.getBoard().getBoard()[shot[0]][shot[1]] = 2;
+
 			oppHit = false;
 			setMessage("Miss!");
+
 		}
 		return oppHit;
 	}
 
+/*
+lossCheck
+checks the values inside of the player's board for remaining unhit ship values
+returns false (opponent has not won) if there are still unhit ships within the player's board
+*/
 	public boolean lossCheck() {
-		
 		boolean oppwin = true;
-		int[][] gameBoard = b.getBoard();
-		//System.out.println("CHECKING!!!!!");
-		
+		int[][] gameBoard = board.getBoard();
+
 		for (int j = 0; j < MAXCOL; j++) {
 			for (int i = 0; i < MAXROW; i++)
 			{
@@ -98,30 +110,58 @@ public abstract class Player {
 		return oppwin;
 	}
 
+/*
+getWin
+returns whether or not player has won
+*/
 	public boolean getWin() {
 		return win;
 	}
 
-	public void setWin(boolean b) {
-		win = b;
+/*
+setWin
+sets the win boolean in player
+*/
+	public void setWin(boolean result) {
+		win = result;
 	}
 
+/*
+getBoard
+returns player's board
+*/
 	public Board getBoard() {
-		return b;
+		return board;
 	}
 
-	public void setDirection(char c){
-		direction=c;
+/*
+setDirection
+sets the direction in player for placing ships
+*/
+	public void setDirection(char aDirection){
+		direction = aDirection;
 	}
 
+/*
+getDirection
+returns the direction set in player for placing ships
+*/
 	public char getDirection(){
 		return direction;
 	}
 
-	public void setMessage(String s){
-		message=s;
+/*
+setMessage
+sets the message in player for player status
+*/
+	public void setMessage(String aMessage){
+		message = aMessage;
 	}
 
+/*
+getMessage
+returns the message saved in player for display
+*/
 	public String getMessage(){
 		return message;
 	}
