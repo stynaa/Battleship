8518cc1c78c1b0d836f4c3e55e6d26a0828afdcf
shawnamckay <shawnammckay@gmail.com>
2017-12-06 14:52:14 -0700

    javadoc-ed

--- BoardSetUpGUI.java
+++ BoardSetUpGUI.java
@@ -7,8 +7,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
+/**
+ * Creates the starting game view, where the user can place their ships
+ */
 
-//creates the starting game GUI view, where the user can place their ships
 public class BoardSetUpGUI extends JPanel {
     
     private JPanel doneResetPanel = new JPanel(); //contains the done and reset buttons
@@ -18,52 +20,77 @@ public class BoardSetUpGUI extends JPanel {
     private JPanel shipSetUpPanel = new JPanel();
     private JPanel difficultyPanel = new JPanel();
 
-    //initializes the human set up view
+
+    /**
+     * Initializes the view for starting the game and setting up the ships
+     * @param human the user's player object
+     * @param listener to use the implemented Action from the Controller class
+     */
     public BoardSetUpGUI(Human human, ActionListener listener){
         
         JPanel content = new JPanel(); //holds the content of the GameStarting screen
         JLabel msg = new JLabel("Welcome to Battleship!");
         playerGrid = new Buttons(human, listener);
+
         setPreferredSize(new Dimension(1000,800));
         setSize(550,550);
         content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
         content.setAlignmentX(Component.CENTER_ALIGNMENT);
         setVisible(true);
+
         msg.setAlignmentX(Component.CENTER_ALIGNMENT);
         content.add(msg);
         content.add(playerGrid);
+
         setDoneResetPanel(listener);
         content.add(doneResetPanel);
+
         directionMsg.setFont(new Font("Serif", Font.BOLD,14));
         directionMsg.setAlignmentX(Component.CENTER_ALIGNMENT);
         content.add(directionMsg);
+
         setShipSetUpPanel(listener);
         content.add(shipSetUpPanel);
+
         setDifficultyPanel(listener);
         content.add(difficultyPanel);
         add(content);
     }
 
-    //accessor for the player's button array that display's the user's board
+
+    /**
+     * accessor for the player's button array that display's the user's board
+     * @return playerGrid button array
+     */
     public Buttons getPlayerGrid() {
         return playerGrid;
     }
 
-    //creates the panel with the direction buttons and the next ship button for the user
+
+    /**
+     * creates the panel with the direction buttons and the next ship button for the user
+     * @param listener to implement the actions declared in the Controller class
+     */
     public void setShipSetUpPanel(ActionListener listener){
-        
         setDirectionPanel(listener);
         shipSetUpPanel.setLayout(new BoxLayout(shipSetUpPanel, BoxLayout.X_AXIS));
+
         shipSetUpPanel.add(directionPanel);
-       shipSetUpPanel.add(Box.createHorizontalGlue());
+        shipSetUpPanel.add(Box.createHorizontalGlue());
+
         JButton shipSet = new JButton("Next Ship");
         shipSet.addActionListener(listener);
         shipSet.setActionCommand("NEXT_SHIP");
+
         shipSetUpPanel.add(shipSet);
         shipSetUpPanel.add(Box.createHorizontalGlue());
     }
 
-    //creates the button options to set the ships, complete ship setup or reset their board.
+
+    /**
+     * creates the button options to set the ships, complete ship setup or reset their board.
+     * @param listener to implement the Actions in the Controller Class
+     */
     public void setDoneResetPanel(ActionListener listener){
         
         doneResetPanel.setLayout(new BoxLayout(doneResetPanel, BoxLayout.X_AXIS));
@@ -72,9 +99,11 @@ public class BoardSetUpGUI extends JPanel {
         JButton done = new JButton("Done");
         done.addActionListener(listener);
         done.setActionCommand("DONE");
+
         JButton reset = new JButton("Reset");
         reset.addActionListener(listener);
         reset.setActionCommand("RESET");
+
         JButton autoSetShips = new JButton("Set Ships For Me");
         autoSetShips.addActionListener(listener);
         autoSetShips.setActionCommand("AUTO_SET_SHIPS");
@@ -88,7 +117,11 @@ public class BoardSetUpGUI extends JPanel {
         doneResetPanel.add(Box.createHorizontalGlue());
     }
 
-    //direction Panel hold the buttons (North, South, East, West) for placing ships.
+
+    /**
+     * direction panel holds the buttons (North, South, East, West) for placing ships.
+     * @param listener to implement the Actions in the Controller Class
+     */
     public void setDirectionPanel(ActionListener listener){
         
         directionPanel.setLayout(new GridLayout(3,3));
@@ -107,7 +140,8 @@ public class BoardSetUpGUI extends JPanel {
         west.addActionListener(listener);
         west.setActionCommand("WEST");
         directionPanel.add(west);
-        directionPanel.add(new JLabel());
+
+        directionPanel.add(new JLabel());//for spacing out the buttons in a nice way
 
         JButton east = new JButton("East");
         east.addActionListener(listener);
@@ -117,31 +151,47 @@ public class BoardSetUpGUI extends JPanel {
         JButton south = new JButton("South");
         south.addActionListener(listener);
         south.setActionCommand("SOUTH");
+
         directionPanel.add(new JLabel()); 
         directionPanel.add(south);
         directionPanel.add(new JLabel()); 
     }
 
-    //creates the panel for the user to select their opponent's difficulty level
+
+    /**
+     * creates the panel for the user to select their opponent's difficulty level
+     * @param listener to implement the Actions in the Controller Class
+     */
     public void setDifficultyPanel(ActionListener listener){
         
         difficultyPanel.setBackground(Color.LIGHT_GRAY);
         JLabel difficultyMessage = new JLabel("Please select your difficulty: ");
         difficultyMessage.setAlignmentX(Component.CENTER_ALIGNMENT);
+
         ButtonGroup group = new ButtonGroup();
+
         JRadioButton easy = new JRadioButton("Easy");
         easy.addActionListener(listener);
         easy.setActionCommand("EASY");
         easy.setSelected(true);
+
         JRadioButton medium =new JRadioButton("Harder");
         medium.addActionListener(listener);
         medium.setActionCommand("HARDER");
+
         group.add(easy);
         group.add(medium);
+
         difficultyPanel.add(difficultyMessage);
         difficultyPanel.add(easy);
         difficultyPanel.add(medium);
     }
+
+
+    /**
+     * Gets the user's name for the the High Score file
+     * @return name string to represent the user's name
+     */
 
     public String getUserName(){
         JOptionPane getName = new JOptionPane();
@@ -150,7 +200,10 @@ public class BoardSetUpGUI extends JPanel {
 
     }
 
-    //mutator for the JLabel directionMsg that sits below the board.
-    public void updateDirectionMsg(String s){directionMsg.setText(s);}
+    /**
+     * mutator for the JLabel directionMsg that sits below the board.
+     * @param message the message to be copied and displayed
+     */
+    public void updateDirectionMsg(String message){directionMsg.setText(message);}
 
 }
