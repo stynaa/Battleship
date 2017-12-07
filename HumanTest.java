904b78efc2200d7bbb7dc94ee004d3f743e9c41c
stynaa <33501062+stynaa@users.noreply.github.com>
2017-12-06 18:32:48 -0700

    Merged HumanTest and PlayerTest
    - added tests in PlayerTest to HumanTest
    - changed the assert equals messages so that they give the proper error message for respective test

--- HumanTest.java
+++ HumanTest.java
@@ -4,20 +4,49 @@ import org.junit.Test;
 
 public class HumanTest{
 
+  //Tests for functionality of Player abstract class through Human subclass
+  @Test
+  public void test_getter_Shot() {
+    Human test = new Human();
+    test.shot[0] = 5;
+    test.shot[1] = 3;
+    assertEquals("Shot y was not copied", 5, test.getShot()[0]);
+    assertEquals("Shot x was not copied", 3, test.getShot()[1]);
+  }
+
+  @Test
+  public void test_privacy_getter_shot() {
+    Human test = new Human();
+    test.shot[0] = 6;
+    test.shot[1] = 8;
+    int[] tempShot = test.getShot();
+    tempShot[0] = 3;
+
+    assertEquals("Privacy leak occurred when retrieving shot coordinates", 6, test.getShot()[0]);
+  }
+
+  @Test
+  public void test_lossCheck_emptyBoard() {
+    Human test = new Human();
+    assertTrue("Empty board, but game is won and over.", test.lossCheck());
+  }
+
+  //Tests for constructors in Human class
   @Test
 	public void testDefaultConstructor(){
         Human h= new Human();
         int[] shot = h.getShot();
-        assertEquals("Test default shot[0]",0 , shot[0]);
-        assertEquals("Test default shot[1]",0, shot[1]);
-        assertEquals("Test default win", false, h.getWin());
-        assertEquals("Test default message", "", h.getMessage());
+        assertEquals("Default of shot[0] is not equal to 0",0 , shot[0]);
+        assertEquals("Default of shot[1] is not equal to 0",0, shot[1]);
+        assertEquals("Default of win boolean is not false", false, h.getWin());
+        assertEquals("Default message is not empty", "", h.getMessage());
     }
 
     //Copy constructor for the Human class should only copy the board and the shot
     @Test
     public void testCopyConstructor(){
           Human h = new Human();
+          h.setDirection('n');
           h.setMessage("hello");
           h.setWin(true);
           int[] hShot= {9,1};
@@ -25,11 +54,12 @@ public class HumanTest{
 
           Human copy= new Human(h);
           int[] shot = copy.getShot();
-          assertEquals("Test copy shot[0]",9 , shot[0]);
-          assertEquals("Test copy shot[1]",1, shot[1]);
-          assertEquals("Test copy win (should be default value)", false, copy.getWin());
-          assertEquals("Test copy message (should be default value)", "", copy.getMessage());
+          assertEquals("Shot[0] within copied Human is not equal to set value",9 , shot[0]);
+          assertEquals("Shot[1] within copied Human is not equal to set value",1, shot[1]);
+          assertEquals("win within copied Human is not the default value (false)", false, copy.getWin());
+          assertEquals("message within copied Human is not the default value (empty)", "", copy.getMessage());
       }
+
 
 
 }
